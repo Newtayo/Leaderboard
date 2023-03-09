9af368e2268bc14f68945512036397a419a91bcd
Newtayo <akandeabdul@gmail.com>
2023-03-09 19:21:54 +0100

    Implement refresh and submit button functionality with data from leaderboard API'

--- src/Modules/renderScore.js
+++ src/Modules/renderScore.js
@@ -1,17 +1,21 @@
-import Score from './Score.js';
 import scoreDisplay from './scoreboard.js';
 
-const render = () => {
-  const player1 = new Score('Tayo', '100');
-  const player2 = new Score('Yemi', '70');
-  const player3 = new Score('Funke', '90');
-  const player4 = new Score('Wale', '80');
-  const player5 = new Score('Tope', '95');
+const render = async () => {
+  const playerScores = async () => {
+    const leaderBoardApi = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wlHvQNs6E7lvVjXzKtfA/scores', {
+      method: 'GET',
 
-  const players = [player1, player2, player3, player4, player5];
+    });
+    const result = await leaderBoardApi.json();
+    return result;
+  };
 
-  players.forEach((player) => {
-    scoreDisplay(player);
+  const player = await playerScores();
+  const scoreList = document.querySelector('.scorelist');
+  scoreList.innerHTML = '';
+  const arrangedList = player.result.sort((a, b) => b.score - a.score);
+  arrangedList.forEach((play) => {
+    scoreDisplay(play);
   });
 };
 export default render;
